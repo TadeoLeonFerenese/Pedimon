5a30ab86c1eedd02907731cbd0fdb6e155f84c3e
Tadeo <teoleon2013@gmail.com>
2021-06-02 20:05:24 -0300

    falta que JS traiga y pinte

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const navToggle = document.querySelector(".nav-toggle");
 const navUL = document.querySelector(".ulBuy");
 
 navToggle.addEventListener("click", () => {
-  navUL.classList.toggle(".ulBuy-visible");
+  navUL.classList.toggle("ulBuy-visible");
 });

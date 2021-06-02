e0655bfe3727e4becaa8af9ccc6a31109d6163ac
Tadeo <teoleon2013@gmail.com>
2021-06-02 18:44:05 -0300

    falta el menu desplegable que funcioneeee

---
+++ index.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navUL = document.querySelector(".ulBuy");
+
+navToggle.addEventListener("click", () => {
+  navUL.classList.toggle(".ulBuy-visible");
+});

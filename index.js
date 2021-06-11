b7d00bd918723f90e82c06970f9b552063959b66
Tadeo <teoleon2013@gmail.com>
2021-06-10 22:38:11 -0300

    faltaria agregar detalles en JS como boton para borrar compra

--- index.js
+++ index.js
@@ -20,17 +20,33 @@ class Pokemon {
   }
 }
 
-var pokemon1 = new Pokemon(`charizard`, `fuego`, "./img/Chorizard.png", 4, 160);
+var pokemon1 = new Pokemon(`charizard`, `fuego`, "./img/Chorizard.png", 3, 160);
 var pokemon2 = new Pokemon(`pikachu`, `Electrico`, "./img/Pikachu.png", 3, 100);
-// var pokemon3 = new Pokemon(`Squirtle`, `Agua`);
-// var pokemon4 = new Pokemon(`Venasaur`, `Hoja`);
-// var pokemon5 = new Pokemon(`Totodile`, `Agua`);
-// var pokemon6 = new Pokemon(`Typhlosion`, `Fuego`);
+var pokemon3 = new Pokemon(
+  `Squirtle`,
+  `Agua`,
+  "./img/vamo a calmarno.png",
+  3,
+  100
+);
+var pokemon4 = new Pokemon(`Venasaur`, `Hoja`, "./img/venusaur.png", 3, 160);
+var pokemon5 = new Pokemon(`Totodile`, `Agua`, "./img/totodile.png", 3, 100);
+var pokemon6 = new Pokemon(
+  `Typhlosion`,
+  `Fuego`,
+  "./img/typhlosion.png",
+  3,
+  160
+);
 
 inventario = [];
 
 inventario.push(pokemon1);
 inventario.push(pokemon2);
+inventario.push(pokemon3);
+inventario.push(pokemon4);
+inventario.push(pokemon5);
+inventario.push(pokemon6);
 
 miCompra = [];
 
